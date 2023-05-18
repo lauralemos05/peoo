6d662acbcf2c1611e0bd1268dcb2483fb607ff59
Laura Lemos <122638808+lauralemos05@users.noreply.github.com>
2023-05-18 11:48:59 -0300

    media entre os numeros

---
+++ questao24.py
@@ -0,0 +1,10 @@
+print('Digite quatro números:')
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+n4 = int(input())
+
+soma = n1 + n2 + n3 + n4 
+media = soma / 4
+
+print('A média entre esses numeros é {}'. format(media))
