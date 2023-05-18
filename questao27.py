1820c6fd11fd349dbc5e4508f18294d1e0fe1918
Laura Lemos <122638808+lauralemos05@users.noreply.github.com>
2023-05-18 11:18:47 -0300

    multiplicação e soma

---
+++ questao27.py
@@ -0,0 +1,11 @@
+print('Digite quatro números:')
+n1 = int(input())
+n2 = int (input())
+n3 = int(input())
+n4 = int(input())
+
+p = n1 * n3
+s = n2 + n4
+
+print( 'O produto do valor n1 com n3 = {}'. format(p))
+print('A soma do valor n2 com o n4 = {}'. format(s))
