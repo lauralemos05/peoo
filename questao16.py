f0c74f7a251c30b8f59e5c0c9176b2ee16bacecf
Laura Lemos <122638808+lauralemos05@users.noreply.github.com>
2023-05-18 10:10:11 -0300

    soma, subtração e multiplicação
    leia dois números e faça a soma, subtração e multiplicação deles, e no final imprima o resultado das três operação

---
+++ questao16.py
@@ -0,0 +1,9 @@
+print('Digite dois números:')
+n1 = int(input())
+n2 = int(input())
+
+s = n1 + n2
+sub = n1 - n2
+mul = n1 * n2
+
+print('A soma entre os números é {}, a subtração é {} e a multiplicação é {}'. format(s, sub,mul))
