44d08a22dc73198a359d0c73872b8443df90e7d0
Laura Lemos <122638808+lauralemos05@users.noreply.github.com>
2023-05-18 10:39:34 -0300

    quatro operação básica
    leia dois números inteiros e realize as 4 operação básica da matemática e depois imprima os resultados

---
+++ questao18.py
@@ -0,0 +1,12 @@
+print('Digite dois números:')
+n1 = int(input())
+n2 = int(input())
+
+s = n1 + n2
+sub = n1 - n2
+mul = n1 * n2
+div = n1 / n2
+
+print('A soma entre esses números é {} e a subtração é {}'. format(s,sub))
+print('A multiplicação {} e a divisão {}'. format(mul,div))
+
