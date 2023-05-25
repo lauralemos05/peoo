f47f203b3acf998f5d2d3a29cb4f74adac1a3bec
lauralemos05 <122638808+lauralemos05@users.noreply.github.com>
2023-05-25 11:17:15 -0300

    volume da lata de óleo

---
+++ questao13.py
@@ -0,0 +1,8 @@
+print('Informe a altura da lata:')
+h = float(input())
+print('Informe o raio da lata:')
+r = float(input())
+
+v = 3.14 * r**2 * h
+
+print('O volume da lata de óleo é {}'.format(v))
