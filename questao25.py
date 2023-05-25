c18c872685699f6f396e3af1657938fd92df3224
lauralemos05 <122638808+lauralemos05@users.noreply.github.com>
2023-05-25 11:05:41 -0300

    volume de uma caixa retangular

---
+++ questao25.py
@@ -0,0 +1,10 @@
+print('Digite o valor do comprimento:')
+c = int(input())
+print('Digite o valor da largura:')
+l = int(input())
+print('Digite o valor da altura:')
+a = int(input())
+
+v = c * l * a
+
+print('O valume da caixa é de', v)
